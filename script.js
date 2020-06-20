66a9032359e7d90e0aec337dffbc231812ceabd1
Apratim Das <apratimdas18@gmail.com>
2020-06-20 15:21:49 +0530

    Another changes

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [90, 92, 93, 85, 80];
 
-window.addEventListener("scroll", () => {
-  mainFn();
-});
+// window.addEventListener("scroll", () => {
+//   mainFn();
+// });
 
 const mainFn = () => {
-  // if (window.pageYOffset >= navbarOffsetTop) {
-  //   navbar.classList.add("sticky");
-  // } else {
-  //   navbar.classList.remove("sticky");
-  // }
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 
-  // sections.forEach((section, i) => {
-  //   if (window.pageYOffset >= section.offsetTop - 10) {
-  //     navbarLinks.forEach((navbarLink) => {
-  //       navbarLink.classList.remove("change");
-  //     });
-  //     navbarLinks[i].classList.add("change");
-  //   }
-  // });
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
 
   if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
     document.querySelectorAll(".progress-percent").forEach((el, i) => {
